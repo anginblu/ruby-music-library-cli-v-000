7d32741cca8d55a329609652d4ececf5d49ce44d
Jacqueline Lee <anginblu@gmail.com>
2017-12-12 01:50:03 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -22,6 +22,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    commond = " "
     while command != "exit"
       puts "What would you like to do?"
       command = gets.strip
